945029ad507d358ea665f62727ad3c7dc748a65e
jhong-tao <jhong.tao@qq.com>
2021-06-20 22:20:24 +0800

    two day

--- Message/apps/message_from/views.py
+++ Message/apps/message_from/views.py
@@ -53,4 +53,17 @@ def message_form(request):
 
         message.save()
 
-    return render(request, 'message_form.html')
+        return render(request, 'message_form.html', {
+                'message': message
+            })
+
+    # 从数据库中取出数据，在HTML展示
+    if request.method == 'GET':
+        var_dict = {}
+        all_message = Message.objects.all()
+        if all_message:
+            message = all_message[0]
+            var_dict = {
+                'message': message
+            }
+        return render(request, 'message_form.html', var_dict)
---
+++ MxOnline/extra_apps/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+==================================================
+@Project -> File   ：MxOnline -> __init__.py
+@IDE    ：PyCharm
+@Author ：jhong.tao
+@Date   ：2021/6/20 22:09
+@Desc   ：
+==================================================
+"""
+
